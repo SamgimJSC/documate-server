503716ffd7acbc80f6ea8927591b6aef16cd6351
seonhyeong8411 <seonhyeong8411@gmail.com>
2026-06-12 17:32:49 +0900

    로그아웃 추가 (#7)
    * 로그아웃 추가
    
    * deco user 추가
    
    ---------
    
    Co-authored-by: ksh <sam2678@naver.com>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signUp.dto';
 import { SendEmailVerificationDto } from './dto/sendEmailVerification.dto';
 import { VerifyEmailVerificationDto } from './dto/verifyEmailVerification.dto';
 import { LoginDto } from './dto/login.dto';
-import { type Response } from 'express';
+import { type Response, type Request } from 'express';
+import { JwtAuthGuard } from './auth.guard';
+import {  ReqUser } from '../global/types/express';
+import { DecoUser } from '../global/decorators/decoUser.decorator';
+import { User } from '../users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -37,5 +41,16 @@ export class AuthController {
       secure: false, // production 에서는 true로 하기
       sameSite: 'lax',
     });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  async logout(
+    @DecoUser() user: User,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    await this.authService.logout(user.userId);
+
+    res.clearCookie('X-Access-Token');
   }
 }
