9fd82632e413f9c9b81f90660e000cd69ed33cce
seonhyeong8411 <seonhyeong8411@gmail.com>
2026-07-23 11:32:43 +0900

    이메일 발송 실패 시 푸시 알림 중복 발송되는 문제 수정 (#61)
    Co-authored-by: choyejin1125 <choyejin1125@gmail.com>

--- src/auth/providors/nodeMailer.ts
+++ src/auth/providors/nodeMailer.ts
@@ -12,8 +12,6 @@ export class NodeMailer {
   async sendEmail(options: SendEmailOption) {
     const { to, subject, code } = options;
 
-    console.log(this.configService.get('NODE_MAILER_PASS'));
-
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
